a8ce4ec5123cfe2a9cf5c72e8cff5195b00540d8
Manuel Leonhardt <manuel@manuelleonhardt.de>
2020-11-08 21:27:16 +0100

    Mirror: 2.0.0

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['oelint_adv==1.27.1'],
+    install_requires=['oelint_adv==2.0.0'],
 )
