94d2558e717ff3340272a1cca2343b8d0c407a0e
pre-commit <pre-commit@example.com>
2021-03-13 17:47:06 +0000

    Mirror: 2.2.3

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['oelint_adv==2.2.2'],
+    install_requires=['oelint_adv==2.2.3'],
 )
